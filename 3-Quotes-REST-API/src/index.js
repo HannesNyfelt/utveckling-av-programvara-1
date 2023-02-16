146812370baa2e6f0bd0b75080f3256143c7a2ed
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-02-16 10:01:03 +0200

    yep

--- 3-Quotes-REST-API/src/index.js
+++ 3-Quotes-REST-API/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+server.use(express.static('public'))
 
+app.get("/api/quote", (req, res) => {
+
+})
 
 app.listen(3000, () => {
     console.log('connected to webb');
