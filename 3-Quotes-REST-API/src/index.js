e6ef289141d4d8aa4bdac58d7b685f1b0e5f5c7d
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-02-16 10:10:43 +0200

    moved things around

--- 3-Quotes-REST-API/src/index.js
+++ 3-Quotes-REST-API/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-server.use(express.static('public'))
+const quotes = require("./data/quotes.json")
+server.use(express.static(__dirname + '/../public'))
 
 app.get("/api/quote", (req, res) => {
-
+    const random = quotes[Math.floor(Math.random() * quotes.length)]
+    res.status(200).send(random)
 })
 
 app.listen(3000, () => {
