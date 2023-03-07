ee948865b3538dcfe67d84bf81fc8a8e06f01ba4
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-03-07 09:35:28 +0200

    somthing not working

---
+++ Dice/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#btn")
+const div = document.querySelector("#div")
+let die = document.createElement("img")
+div.append(die)
+
+btn.addEventListener("click", function () {
+
+})
