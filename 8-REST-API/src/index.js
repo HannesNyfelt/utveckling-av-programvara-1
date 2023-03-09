4aff7cdb8bab96d7b77fc6c9235ae6a7451ba9f6
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-03-09 12:20:55 +0200

    yup

--- 8-REST-API/src/index.js
+++ 8-REST-API/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const mysql = require('mysql2')
-
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'pizzaTower'
+})
 const app = express()
 app.use(express.static(__dirname + '/../public'))
 
 
-
+app.get('/api/pizza', async (req, res) => {
+    const [rows] = await connection.promise().query('SELECT * FROM pizza')
+    res.json(rows)
+})
 
 
 app.listen(3000, () => {
