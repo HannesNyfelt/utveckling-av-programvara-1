5d27a6abef786695a898d8b71766e2582e13c465
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-03-15 13:10:15 +0200

    done

--- Dice/app.js
+++ Dice/app.js
@@ -1,9 +1,15 @@
 const btn = document.querySelector("#btn")
 const div = document.querySelector("#div")
 let die = document.createElement("img")
+let die2 = document.createElement("img")
 
 
 btn.addEventListener("click", function () {
     div.append(die)
-    die.src = "images/die#3.png"
+    const num = Math.floor(Math.random() * 5 + 1)
+    die.src = `images/die${num}.png`
+
+    div.append(die2)
+    const num2 = Math.floor(Math.random() * 5 + 1)
+    die2.src = `images/die${num2}.png`
 })
